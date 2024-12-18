36424e8c584bdd192fc6c290fbe02900c548298d
nanugo_YB <ybson11@nanugo.kr>
2024-12-18 17:09:30 +0900

    코멘트 라우팅 추가

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegionController;
 use App\Http\Controllers\GalleryController;
 
+
+//로그인
 Route::post('/register', [AuthController::class, 'register'] );
 Route::post('/login', [AuthController::class, 'login']);
 
+
+//지역 라우팅
 Route::get('/regions', [RegionController::class, 'regions']);
 
+
+//갤러리 라우팅
 Route::get('/regions/galleryList', [GalleryController::class, 'galleryList']);
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/regions/gallery', [GalleryController::class, 'galleryAdd']);
@@ -18,6 +24,8 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::delete('/regions/gallery', [GalleryController::class, 'galleryDelete']);
 });
 
+
+//게시글 라우팅
 Route::get('/regions/gallery/postList', [GalleryController::class, 'postList']);
 Route::get('/regions/gallery/post', [GalleryController::class, 'viewPost']);
 Route::group(['middleware' => 'auth:api'], function () {
@@ -29,4 +37,17 @@ Route::group(['middleware' => 'auth:api'], function () {
 Route::group(['middleware' => 'auth:api'], function () {
     Route::put('/regions/gallery/post', [GalleryController::class, 'postUpdate']);
 });
+
+//코멘트 라우팅
+Route::get('/regions/gallery/post/comments', [GalleryController::class, 'viewComment']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('/regions/gallery/post/comments', [GalleryController::class, 'postComment']);
+});
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::delete('/regions/gallery/post/comments', [GalleryController::class, 'commentDelete']);
+});
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::put('/regions/gallery/post/comments', [GalleryController::class, 'commentUpdate']);
+});
+
 ?>
